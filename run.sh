c956d6003708ee82e9f878ff3985f86b727b73f8
LinYushu <72435115+LinYushu@users.noreply.github.com>
2026-03-27 21:20:10 +0800

    chore: 补充启动脚本等

--- run.sh
+++ run.sh
@@ -9,22 +9,22 @@ src_count=$(find src  -type f \( -name "*.cpp" -o -name "*.h" -o -name "*.txt" \
 total=$((include_count + src_count))
 
 if [ ! -d "data/debug" ]; then
-    mkdir data/debug
+    mkdir -p data/debug
     touch data/debug/here_save_debug_images
 fi
 
 if [ ! -d "data/video" ]; then
-    mkdir data/video
+    mkdir -p data/video
     touch data/video/here_save_video
 fi
 
 if [ ! -d "data/speed" ]; then
-    mkdir data/speed
+    mkdir -p data/speed
     touch data/speed/here_save_shoot_speed
 fi
 
 if [ ! -d "/etc/openrm" ]; then 
-    mkdir /etc/openrm
+    mkdir -p /etc/openrm
     sudo cp -r data/uniconfig/* /etc/openrm/
     sudo chmod -R 777 /etc/openrm
 fi
@@ -39,8 +39,10 @@ if [ ! -d "build" ]; then
 fi
 
 imshow=0
+usensys=0
+skip_build=0
 
-while getopts ":rcg:ls" opt; do
+while getopts ":rcg:lsnk" opt; do
     case $opt in
         r)
             echo -e "${yellow}<--- delete 'build' --->\n${reset}"
@@ -77,6 +79,14 @@ while getopts ":rcg:ls" opt; do
             imshow=1
             shift
             ;;
+        n)
+            usensys=1
+            shift
+            ;;
+        k)
+            skip_build=1
+            shift
+            ;;
         \?)
             echo -e "${red}\n--- Unavailable param: -$OPTARG ---\n${reset}"
             ;;
@@ -87,14 +97,18 @@ while getopts ":rcg:ls" opt; do
     done
 
 
-echo -e "${yellow}<--- Start CMake --->${reset}"
-cd build
-cmake ..
+cd build # 无论是否编译，都需要进入 build 目录以供后续复制可执行文件
 
+if [ $skip_build -eq 0 ]; then
+    echo -e "${yellow}<--- Start CMake --->${reset}"
+    cmake ..
 
-echo -e "${yellow}\n<--- Start Make --->${reset}"
-max_threads=$(cat /proc/cpuinfo | grep "processor" | wc -l)
-make -j "$max_threads"
+    echo -e "${yellow}\n<--- Start Make --->${reset}"
+    max_threads=$(cat /proc/cpuinfo | grep "processor" | wc -l)
+    make -j "$max_threads"
+else
+    echo -e "${yellow}\n<--- Skip CMake & Make (Using existing binaries) --->${reset}"
+fi
 
 
 echo -e "${yellow}\n<--- Total Lines --->${reset}"
@@ -112,7 +126,7 @@ trap restore_performance EXIT
 echo -e "${yellow}\n<--- Enabling Max Performance Mode --->${reset}"
 sudo nvpmodel -m 8
 
-if [ ! -f /root/.jetsonclocks_conf.txt ]; then
+if ! sudo test -f /root/.jetsonclocks_conf.txt; then
     echo -e "${blue}Storing default jetson_clocks state...${reset}"
     sudo jetson_clocks --store
 fi
@@ -120,16 +134,35 @@ fi
 sudo jetson_clocks --fan
 
 echo -e "${yellow}\n<--- Run Code --->${reset}"
-sudo rm /usr/local/bin/TJURM-2024
+sudo rm -f /usr/local/bin/TJURM-2024
 sudo cp TJURM-2024 /usr/local/bin/
 sudo pkill TJURM-2024
 sudo chmod 777 /dev/tty*
 
-if [ $imshow = 1 ]; then
-    TJURM-2024 -s
+if [ $usensys = 1 ]; then
+    echo -e "${yellow}\n<--- Start Nsight Systems Profiling --->${reset}"
+    if [ $imshow = 1 ]; then
+        sudo nsys profile \
+            --trace=cuda,nvtx,osrt \
+            -y 10 -d 10 \
+            --output=autoaim_nsys \
+            --force-overwrite=true \
+            TJURM-2024 -s
+    else
+        sudo nsys profile \
+            --trace=cuda,nvtx,osrt \
+            -y 10 -d 10 \
+            --output=autoaim_nsys \
+            --force-overwrite=true \
+            TJURM-2024
+    fi
 else
-    TJURM-2024
-
+    echo -e "${yellow}\n<--- Start Program (Normal Mode) --->${reset}"
+    if [ $imshow = 1 ]; then
+        sudo TJURM-2024 -s
+    else
+        sudo TJURM-2024
+    fi
 fi
 
 /etc/openrm/guard.sh
